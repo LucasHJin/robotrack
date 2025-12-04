48230e3c1d7735a6c2335297339e4f159c794ed4
Lucas Jin <kblazer20@gmail.com>
2025-12-04 00:07:04 -0500

    perf: tuned hyperparameters for yolo

--- train.py
+++ train.py
@@ -1,5 +1,13 @@
 import modal
 
+"""
+first model
+    prediction: 95.3
+    recall: 91.3
+    mAP50: 96.4
+    mAP50-95: 0.703
+"""
+
 image = modal.Image.debian_slim().apt_install(
         "libgl1-mesa-glx", # need these libraries for opencv
         "libglib2.0-0",
@@ -16,26 +24,29 @@ app = modal.App("frc-training", image=image)
 volume = modal.Volume.from_name("frc", create_if_missing=True)
 
 
-@app.function(volumes={"/data": volume}, gpu="L40S", timeout=43200, retries=1)
+@app.function(volumes={"/data": volume}, gpu="L40S", timeout=36000, retries=1)
 def train():
     from ultralytics import YOLO
 
-    model = YOLO("yolov8l.pt")
+    model = YOLO("yolov8x.pt")
 
     results = model.train(
         data="/data/dataset/data.yaml",
-        epochs=80,
-        imgsz=640,
-        batch=32,
+        epochs=120,
+        imgsz=1280,
+        batch=16,
         project="/data/runs",
-        name="frc-v1",
-        patience=10,
+        name="frc-v2",
+        patience=15,
         
         #augmentation
         degrees=5,
         shear=2,
-        mixup=0.05,
-        hsv_v=0.6,
+        mixup=0.1,
+        hsv_v=0.5,
+        hsv_h=0.03,
+        hsv_s=0.75,
+        copy_paste=0.1
     )
     
     volume.commit()
