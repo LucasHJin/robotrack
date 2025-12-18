923963132b4e5af080ca3eac3a2cc7eee6ae95f4
Lucas Jin <kblazer20@gmail.com>
2025-12-17 21:30:27 -0500

    chore: add field for homography

--- homography.py
+++ homography.py
@@ -1,8 +1,7 @@
 
 # Steps
     # Field detection (reference points)
-    # Homography computation -> 3x3 matrix
+    # Homography computation -> 3x3 matrix - use open cv
     # Convert pixel coords to field coords (throughout game)
     # Visualization
-    
     
